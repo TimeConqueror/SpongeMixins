85d48448093ceb8534821167777c55a52893c870
TimeConqueror <timeconqueror999@gmail.com>
2020-02-17 21:04:04 +0300

    Fixed to work in dev-workspace

--- src/main/java/ru/timeconqueror/spongemixins/SpongeMixins.java
+++ src/main/java/ru/timeconqueror/spongemixins/SpongeMixins.java
@@ -1,7 +1,12 @@
 package ru.timeconqueror.spongemixins;
 
 import cpw.mods.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-@Mod(modid = "spongemixins", version = "1.0.0")
+@Mod(modid = SpongeMixins.MODID, version = "1.0.0", name = SpongeMixins.NAME)
 public class SpongeMixins {
+    public static final String NAME = "SpongeMixins Loader";
+    public static final String MODID = "spongemixins";
+    public static final Logger LOGGER = LogManager.getLogger(NAME);
 }
