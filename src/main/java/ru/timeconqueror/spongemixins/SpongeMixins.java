a8f81842ea7d7cf131191ea41ba58c3cb05b9a3c
Jason Mitchell <mitchej@gmail.com>
2021-01-10 15:46:19 -0800

    Add utility to manipulate the Minecraft URL Class Path

--- src/main/java/ru/timeconqueror/spongemixins/SpongeMixins.java
+++ src/main/java/ru/timeconqueror/spongemixins/SpongeMixins.java
@@ -4,7 +4,7 @@ import cpw.mods.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = SpongeMixins.MODID, version = "1.2.0", name = SpongeMixins.NAME)
+@Mod(modid = SpongeMixins.MODID, version = "1.3.0", name = SpongeMixins.NAME)
 public class SpongeMixins {
     public static final String NAME = "SpongeMixins Loader";
     public static final String MODID = "spongemixins";
